5e58d8ad9bcb1af778a8f4988de8c9d50352793d
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-07-14 14:24:16 -0300

    REQUISITO 2: criando o modelo user.js

---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+const sequelize = require('sequelize');
+
+const createUser = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'Users',
+  });
+
+  return User;
+  }
+
+
+
+  module.exports = createUser;
